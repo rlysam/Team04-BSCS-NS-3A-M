aa04f9766b3addc2d094ae910ea586630e61fec9
marvinraydalida <marvinraydalida@gmail.com>
2022-01-15 22:20:18 +0800

    Added added image_chat type and image upload

--- application/controllers/Pahiram_chat.php
+++ application/controllers/Pahiram_chat.php
@@ -1,11 +1,21 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 header('Access-Control-Allow-Origin: *');
 
-class Pahiram_chat extends CI_Controller {
-    
-  public function send_message(){
+class Pahiram_chat extends CI_Controller
+{
+
+  public function send_message()
+  {
     $this->load->model('Pahiram_chat_model');
     $this->Pahiram_chat_model->store_message();
+  }
+
+  public function get_image()
+  {
+    $this->load->helper('file');
+    $filename = $this->input->get('path');
+    header('Content-type: ' . get_mime_by_extension($filename));
+    echo file_get_contents($filename);
   }
 }
--- application/controllers/Pasabay_chat.php
+++ application/controllers/Pasabay_chat.php
@@ -9,6 +9,13 @@ class Pasabay_chat extends CI_Controller
     {
         $this->load->model('Pasabay_chat_model');
         $this->Pasabay_chat_model->store_message();
+        echo "heloo";
+    }
 
+    public function get_image(){
+        $this->load->helper('file');
+        $filename = $this->input->get('path');
+        header('Content-type: ' . get_mime_by_extension($filename));
+        echo file_get_contents($filename); 
     }
 }
