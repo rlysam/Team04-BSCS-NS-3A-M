d6f088694bdc83237e720279ded46d4cf3a949fa
marvinraydalida <marvinraydalida@gmail.com>
2022-01-29 14:27:55 +0800

    Minor change

--- application/controllers/Pahiram_post.php
+++ application/controllers/Pahiram_post.php
@@ -3,13 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 header('Access-Control-Allow-Origin: *');
 
 class Pahiram_post extends CI_Controller {
-    
-    private $STATUS_AVAILABLE = 'available';
-    private $STATUS_UNAVAILABLE = 'unavailable';
-    private $STATUS_RENTING = 'renting';
-    private $STATUS_REQUESTING = 'requesting';
-    private $STATUS_DEACTIVATED = 'deactivated';
-
 
     public function get_post() {
 
@@ -47,34 +40,9 @@ class Pahiram_post extends CI_Controller {
         echo file_get_contents($filename); 
     }
 
-    public function set_post_available($post_id) { 
-        $this->load->model('Pahiram_post_model');
-        $status_code = $this->Pahiram_post_model->set_status($post_id, $this->STATUS_AVAILABLE);
-        $this->output->set_status_header($status_code);
-    }
-
-    public function set_post_unavailable($post_id) {
-        $this->load->model('Pahiram_post_model');
-        $status_code = $this->Pahiram_post_model->set_status($post_id, $this->STATUS_UNAVAILABLE);
-        $this->output->set_status_header($status_code);
-    }
-
-    public function set_post_renting($post_id) {
-        $this->load->model('Pahiram_post_model');
-        $status_code = $this->Pahiram_post_model->set_status($post_id, $this->STATUS_RENTING);
-        $this->output->set_status_header($status_code);
-    }
-
-    public function set_post_requesting($post_id) {
-        $this->load->model('Pahiram_post_model');
-        $status_code = $this->Pahiram_post_model->set_status($post_id, $this->STATUS_REQUESTING);
-        $this->output->set_status_header($status_code);
-    }
-
-
     public function deactivate_post() {
         $this->load->model('Pahiram_post_model');
-        $status_code = $this->Pahiram_post_model->set_status($_POST['post_id'], $this->STATUS_DEACTIVATED);
+        $status_code = $this->Pahiram_post_model->set_status($_POST['post_id']);
         $this->output->set_status_header($status_code);
     }
     // public function deactivate_post($post_id) {
